599e84075b55d4cba5701a72758a8a02b988c252
surajdev10 <surajdevfb@gmail.com>
2023-01-20 00:36:13 +0530

    Store daily wage

--- EmployeeWageProgram.js
+++ EmployeeWageProgram.js
@@ -86,3 +86,29 @@ const WAGE_PER_HOUR = 20;
     console.log( "UC5--- Total Days:" + totalWorkingDays +
              " Total Hrs:" +  totalEmpHrs + " EmpWage: " +  empWage);
 }
+          
+//UC6
+function calcDailyWage(empHrs)
+{
+    return empHrs * WAGE_PER_HOUR;
+}
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+{
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+empWage = calcDailyWage(totalEmpHrs);
+console.log("Employee wage array: " + empDailyWageArr.join(" "));
+console.log("UC6--- Total Days: " + totalWorkingDays +
+      " Total Hrs: " + totalEmpHrs +
+      " Emp Wage: " + empWage);
+
+   
