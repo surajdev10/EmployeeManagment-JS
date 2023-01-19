1e19f9f7ee23b4ef5e5684af69fd6f912f67c179
surajdev10 <surajdevfb@gmail.com>
2023-01-20 00:30:30 +0530

    Employee Wage with condition of working hours and month

--- EmployeeWageProgram.js
+++ EmployeeWageProgram.js
@@ -70,3 +70,19 @@ const WAGE_PER_HOUR = 20;
                  " Total Hrs:" +totalEmpHrs + " Emp Wage:" + empWage  );
 }
 
+//UC5
+{
+    const MAX_HRS_IN_MONTH = 160;
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH &&  totalWorkingDays < NUM_OF_WORKING_DAYS)
+    {
+       totalWorkingDays++;
+       let empCheck = Math.floor(Math.random() * 10) % 3;
+       totalEmpHrs += getWorkingHours(empCheck);
+    }
+    empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log( "UC5--- Total Days:" + totalWorkingDays +
+             " Total Hrs:" +  totalEmpHrs + " EmpWage: " +  empWage);
+}
