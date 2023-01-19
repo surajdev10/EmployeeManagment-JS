078385fd1e7d1a4f4591033fbc83dcb01d464f8b
surajdev10 <surajdevfb@gmail.com>
2023-01-20 00:49:18 +0530

    Ability to store the Day, Hours Worked and Wage Earned in a single object

--- EmployeeWageProgram.js
+++ EmployeeWageProgram.js
@@ -230,3 +230,24 @@ console.log("UC8 - Total wage by Map is: " + totalWage);
   console.log("Part Working Days: " + partWorkingDays);
   console.log("Non Working Days: " + nonWorkingDays);
 }
+
+// UC10 .
+{
+  const MAX_HRS_IN_MONTH = 160;
+  const NUM_OF_WORKING_DAYS = 20;
+  let totalEmpHrs = 0;
+  let totalWorkingDays = 0;
+  let empDailyHrsAndWageArr = new Array();
+  while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+  {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyHrsAndWageArr.push({dayNum: totalWorkingDays,dailyHours: empHrs,dailyWage: calcDailyWage(empHrs),toString()
+    {
+      return ("\nDay" + this.dayNum + " => Working Hours is " + this.dailyHours + " And Wage Earned = " + this.dailyWage);
+    },});
+  }
+  console.log("\nUC10 Showing Daily Hours Worked and Wage Earned: " + empDailyHrsAndWageArr);
+}
